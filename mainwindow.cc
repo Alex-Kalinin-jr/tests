d1273c052b1a0d0839479c720e62cda443dcb8f9
Alexey <alexey@debian.pro>
2023-05-07 18:18:27 +0700

    initial commit. sketches of tables were created. db connection was established.

---
+++ mainwindow.cc
@@ -0,0 +1,31 @@
+﻿#include "mainwindow.h"
+
+#include <QDebug>
+#include <QSqlQuery>
+
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  dBase_ = (QSqlDatabase::addDatabase("QMYSQL"));
+  dBase_.setHostName("localhost");
+  dBase_.setDatabaseName("questions");
+  dBase_.setUserName("student");
+  dBase_.setPassword("student");
+  bool ok = dBase_.open();
+  if (ok) {
+    qDebug("dbase is opened");
+    QSqlQuery query("select * from question");
+    while (query.next()) {
+      QString a =
+          query.value(0).toString() + "-->>" + query.value(2).toString();
+      qDebug() << a;
+    }
+  } else {
+    qDebug("smthng got wrong!");
+  }
+}
+
+MainWindow::~MainWindow() { delete ui; }
